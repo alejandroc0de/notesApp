18b77ed8150acf8ff1c9ff7206e3ae1f4b9cba03
Alejandro <carrilloalejandro0215@gmail.com>
2026-05-15 19:18:07 -0500

    Working on routes

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import findAll from '../services/notesService.js'
 import dataAccess from '../services/notesService.js';
 const router = express.Router();
 
@@ -9,6 +8,17 @@ router.get("/", async (req,res) => {
         res.send(notes)
     } catch (error) {
         console.log(error)
+    }
+})
+
+router.post("/", async (req,res)=> {
+    try {
+        const note = req.body.note
+        const title = req.body.title
+        const id = req.body.title
+        
+    } catch (error) {
+        
     }
 })
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import { useEffect } from 'react'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
 
 
+  const [recentNotes, setRecentNotes] = useState([])
+  const [note, setNote] = useState("")
 
   // function to load all the notes from the back 
   useEffect( () => {
@@ -11,7 +14,7 @@ function App() {
       try {
         const result = await fetch(`${import.meta.env.VITE_API_URL}/notas`)
         const data = await result.json()
-        console.log(data)
+        setRecentNotes(data)
       } catch (error) {
         console.log(error)
       }
@@ -19,11 +22,40 @@ function App() {
     getNotes()
   },[])
 
+  function handleSubmit(){
+    console.log(note)
+  }
+
+  function handleNote(event){
+    setNote(event.target.value)
+  }
+
   return (
     <>
       <div>
-        <h1>Notes App</h1>
+        <h1 className='text-4xl'>Notes App</h1>
       </div>
+
+      <div id='showNotes'>
+        {recentNotes && <div>
+          {recentNotes.map((item,index)=>(
+            <div key={index}>
+              <p>{item.Nota}</p>
+            </div>
+          ))}          
+          </div>}
+      </div>
+
+      <div className='bg-green-200 p-5 flex flex-col' id='enterNotes'>
+        <label for="message" class=" block mb-2.5 text-sm font-medium text-heading">Your Note..</label>
+        <textarea value={note} onChange={handleNote} id="message" rows="4" class="m-5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand w-[90%] h-fit p-3.5 shadow-xs placeholder:text-body" placeholder="Write your thoughts here..."></textarea>
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+
+
+
+
+
     </>
   )
 }
